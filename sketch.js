f28eb0c3598c106f4359a746f1d356b35045b600
Daniel Nascimento <danielhn@gmail.com>
2020-07-10 23:51:39 +0000

    Just a file for tinkering js

---
+++ sketch.js
@@ -0,0 +1,25 @@
+const largeSphere = {
+  type: 'sphere',
+  radius: 40
+}
+
+const smallSphere = {
+  type: 'sphere',
+  radius: 10
+}
+
+const cone = {
+  type: 'cone',
+  radius: 3,
+  height: 5
+}
+
+const duck = [
+  largeSphere,
+  smallSphere,
+  cone
+]
+
+for (let i = 0; i < duck.length; i++) {
+  console.log(duck[i].type);
+}
