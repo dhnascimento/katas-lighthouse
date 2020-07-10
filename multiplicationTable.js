b40e8affcf46699c4d68ec196ecc98eb222b5060
Daniel Nascimento <danielhn@gmail.com>
2020-07-10 17:23:24 +0000

    Multiplication table solution.

---
+++ multiplicationTable.js
@@ -0,0 +1,18 @@
+const multiplicationTable = function(maxValue) {
+  let table = "";
+  for (let i = 1; i <= maxValue; i++) {
+    for (let j = 1; j <= maxValue; j++) {
+      table += (i * j).toString();
+      if (j === maxValue) {
+        table += "\n";
+      } else {
+        table += " "
+      }
+    }
+  } return table;
+};
+
+console.log(multiplicationTable(1));
+console.log(multiplicationTable(5));
+console.log(multiplicationTable(10));
+
